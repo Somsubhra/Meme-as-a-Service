6c7638d9d15cde6e228a5e9c3279c754927c4675
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-06-07 23:54:32 +0530

    Try styling the meme page

--- MAASApi/MAASApi.py
+++ MAASApi/MAASApi.py
@@ -1,3 +1,9 @@
+# The image dictionary
+IMAGE_DICT = {
+    "obama": "/static/img/obama.jpg"
+}
+
+
 # The Meme as a service API
 class MAASApi:
 
@@ -5,13 +11,21 @@ class MAASApi:
     def __init__(self):
         pass
 
+    # Get the image URL
+    @staticmethod
+    def image_url(img):
+        if img in IMAGE_DICT:
+            return IMAGE_DICT[img]
+        else:
+            return img
+
     # Handler for the API
     @staticmethod
     def handler(jinja_env, img, top_caption=None, bottom_caption=None):
         template_varibles = {
             "bottom_caption": bottom_caption,
             "top_caption": top_caption,
-            "img_url": img
+            "img_url": MAASApi.image_url(img)
         }
 
         template = jinja_env.get_template("api.html")
